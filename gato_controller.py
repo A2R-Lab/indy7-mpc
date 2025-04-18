e4b6b6f46dcd3566718b7ce6689851bd5266620e
Alex Du <alexander.du@columbia.edu>
2025-04-17 21:09:18 -0400

    upgrades

--- gato_controller.py
+++ gato_controller.py
@@ -1,4 +1,5 @@
 import numpy as np
+from datetime import datetime
 import torch
 import sys 
 import os
@@ -14,8 +15,10 @@ from geometry_msgs.msg import PoseStamped, Wrench
 np.random.seed(42)
 # timeout duration for joint state subscriber
 JOINT_STATE_TIMEOUT = 10.0
+# interval for saving statistics (in seconds)
+STATS_SAVE_INTERVAL = 15.0
 
-def figure8(A_x=0.4, A_z=0.4, offset=[0.0, 0.50, 0.7], period=5, dt=0.01, cycles=1):
+def figure8(A_x=0.4, A_z=0.4, offset=[0.0, 0.5, 0.6], period=5, dt=0.01, cycles=1):
     x = lambda t: offset[0] + A_x * np.sin(t)  # X goes from -xamplitude to xamplitude
     y = lambda t: offset[1]
     z = lambda t: offset[2] + A_z * np.sin(2*t)/2 + A_z/2  # Z goes from -zamplitude/2 to zamplitude/2
@@ -25,7 +28,7 @@ def figure8(A_x=0.4, A_z=0.4, offset=[0.0, 0.50, 0.7], period=5, dt=0.01, cycles
     return np.tile(fig_8, cycles)
 
 class GATO_Batch_Sample:
-    def __init__(self, N=32, dt=0.01, batch_size=4, stats=None, f_ext_std=1.0):
+    def __init__(self, N=32, dt=0.01, batch_size=4, stats=None, f_ext_std=1.0, f_ext_resample_std=0.0):
         self.N = N
         self.dt = dt
         
@@ -52,26 +55,25 @@ class GATO_Batch_Sample:
             'pcg_iters': {'values': [], 'unit': '', 'multiplier': 1},
             "step_size": {"values": [], "unit": "", "multiplier": 1}
         }
-        # if f_ext_std != 0.0:
-        self.f_ext_batch = np.random.normal(0, f_ext_std, (self.batch_size, 6))
-        self.f_ext_batch[:, 3:] = 0.0
-        self.f_ext_batch[0] = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])  
-        # else:
-        #     self.f_ext_batch = np.zeros((self.batch_size, 6))
+
+        if f_ext_std != 0.0:
+            self.f_ext_batch = np.random.normal(0, f_ext_std, (self.batch_size, 6))
+            self.f_ext_batch[:, 3:] = 0.0
+            self.f_ext_batch[0] = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])  
+        else:
+            self.f_ext_batch = np.zeros((self.batch_size, 6))
+
+        self.resample_f_ext = True if f_ext_resample_std > 0.0 else False
+        self.f_ext_resample_std = f_ext_resample_std
         
         print("f_ext_batch:")
-        print(self.f_ext_batch)
+        print(np.array2string(self.f_ext_batch, precision=4, suppress_small=True))
         self.solver.set_external_wrench_batch(self.f_ext_batch)
-        
-        
+
         
     def solve(self, xcur_batch, eepos_goals_batch, XU_batch):
-        # xcur_batch: (batch_size, state_size)
-        # eepos_goals_batch: (batch_size, N * 6)
-        # XU_batch: (batch_size, N*(state_size+control_size)-control_size)
         
         result = self.solver.solve(XU_batch, self.dt, xcur_batch, eepos_goals_batch)
-
         self.stats['solve_time']['values'].append(result["solve_time_us"])
         self.stats['sqp_iters']['values'].append(result["sqp_iterations"])
         for i in range(len(result["pcg_stats"])):
@@ -79,12 +81,33 @@ class GATO_Batch_Sample:
         for i in range(len(result["line_search_stats"])):
             self.stats["step_size"]["values"].append(result["line_search_stats"][i]["step_size"])
             
-        return result["xu_trajectory"]
+        return result["xu_trajectory"], result["solve_time_us"]
     
-    def sim_forward(self, xk, uk, sim_dt):
-        x_next_batch = self.solver.sim_forward(xk, uk, sim_dt)
+    def sim_forward(self, x, u, sim_dt):
+        x_next_batch = self.solver.sim_forward(x, u, sim_dt)
         return x_next_batch
     
+    def find_best_idx(self, x_last, u_last, xs, dt):
+        best_error, best_idx = np.inf, None
+        x_next_batch = self.solver.sim_forward(x_last, u_last, dt)
+        
+        for i in range(self.batch_size):
+            error = np.linalg.norm(x_next_batch[i, :] - xs)
+            if error <= best_error:
+                best_error, best_idx = error, i
+                
+        return best_idx
+    
+    def resample_f_ext_batch(self, best_idx):
+        if self.resample_f_ext:
+            f_ext_best = self.f_ext_batch[best_idx, :]
+            self.f_ext_batch[:, :] = f_ext_best
+            self.f_ext_batch = np.random.normal(self.f_ext_batch, self.f_ext_resample_std, self.f_ext_batch.shape)
+            self.f_ext_batch[:, 3:] = 0.0
+            self.f_ext_batch[best_idx] = f_ext_best
+            self.f_ext_batch[0] = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+            self.solver.set_external_wrench_batch(self.f_ext_batch)
+
     def reset(self):
         self.solver.reset()
         
@@ -98,177 +121,154 @@ class GATO_Batch_Sample:
         return self.stats
 
 
-
-
-
 class GATO_Controller(Node):
-    def __init__(self, ref_traj=None, batch_size=1, N=32, dt=0.01, f_ext_std=0.0, f_ext_resample_std=0.5, f_ext_actual=None):
+    def __init__(self, ref_traj=None, batch_size=1, N=32, dt=0.01, 
+                 f_ext_std=0.0, f_ext_resample_std=0.0, 
+                 f_ext_actual=None):
+        
+        super().__init__('gato_controller')
         
         if ref_traj is None:
             raise ValueError("ref_traj must be provided")
         self.ref_traj = ref_traj
         self.ref_traj_offset = 0
-        
-        super().__init__('gato_controller')
-        self.subscription = self.create_subscription(
-            JointState,
-            'joint_states',
-            self.joint_callback,
-            1)
-        
-        self.publisher = self.create_publisher(
-            JointState, 
-            'joint_commands', 
-            1)
-        
-        self.external_force_publisher = self.create_publisher(
-            Wrench,
-            'external_force',
-            1)
-        
-        self.ctrl_msg = JointState()
-        self.ctrl_msg.name = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
-        
+
         self.batch_size = batch_size
         self.N = N
         self.dt = dt
         self.nu, self.nx = 6, 12
         self.config = f"dt:{self.dt}_batch_size:{self.batch_size}_N:{self.N}_f_ext_std:{f_ext_std}"
+
+
+        self.subscription = self.create_subscription(JointState, 'joint_states', self.joint_callback, 1)
         
-        self.solver = GATO_Batch_Sample(self.N, self.dt, self.batch_size, f_ext_std=f_ext_std)
-        self.f_ext_resample_std = f_ext_resample_std
+        self.publisher = self.create_publisher(JointState, 'joint_commands', 1)
+        
+        self.external_force_publisher = self.create_publisher(Wrench, 'external_force', 1)
+        
+        self.ctrl_msg = JointState()
+        self.ctrl_msg.name = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6']
+        self.ctrl_msg.position = [0.0] * 6 
+        self.ctrl_msg.velocity = [0.0] * 6
+        
+        self.solver = GATO_Batch_Sample(self.N, self.dt, self.batch_size, f_ext_std=f_ext_std, f_ext_resample_std=f_ext_resample_std)
+
+        f_ext_actual = f_ext_actual if f_ext_actual is not None else np.zeros(6)
+        self.send_external_force(f_ext_actual)
     
-        # initialize
+        # initialize inputs
         self.ee_pos_traj_batch = np.tile(self.ref_traj[:6*self.N], (self.batch_size, 1))
         XU_batch = np.tile(np.zeros(N*18-6), (self.solver.batch_size, 1))
         self.xs_batch = np.tile(np.zeros(12), (self.solver.batch_size, 1))
-        self.XU_batch = self.solver.solve(self.xs_batch, self.ee_pos_traj_batch, XU_batch)
+        self.XU_batch, _ = self.solver.solve(self.xs_batch, self.ee_pos_traj_batch, XU_batch)
         self.XU_best = self.XU_batch[0, :]
-        
-        self.x_last = None
-        self.u_last = None
-        self.resample_f_ext = True
+        self.x_last, self.u_last = None, None
         
         # stats
-        self.delta_ts = []
+        self.dts = []
         self.tracking_errors = []
         self.ee_positions = []
+        self.ee_ref_positions = []
         self.joint_positions = []
-        
-        if f_ext_actual is not None:
-            self.f_ext = f_ext_actual
-        else:
-            self.f_ext = np.zeros(6)
-            
-        self.send_external_force()
-        
+        self.solve_times = []
+    
         # timing
         self.timeout_timer = self.create_timer(1.0, self.check_timeout)
         self.last_time = time.time()
-        
-        
-    def find_best_trajectory_id(self, x_last, u_last, xs, dt):
-        best_error = np.inf
-        best_idx = None
-        
-        # self.solver.solver.set_external_wrench_batch(self.solver.f_ext_batch)
-        x_next_batch = self.solver.sim_forward(x_last, u_last, dt)
-        
-        for i in range(self.solver.batch_size):
-            x_next = x_next_batch[i, :]
-            error = np.linalg.norm(x_next - xs)
-            
-            if error <= best_error:
-                best_error = error
-                best_idx = i
-                
-        if self.resample_f_ext:
-            f_ext_best = self.solver.f_ext_batch[best_idx, :]
-            self.solver.f_ext_batch = np.tile(f_ext_best, (self.solver.batch_size, 1))
-            self.solver.f_ext_batch += np.random.normal(0, self.f_ext_resample_std, self.solver.f_ext_batch.shape)
-            self.solver.f_ext_batch[:, 3:] = 0.0
-            self.solver.f_ext_batch[best_idx] = f_ext_best
-            self.solver.f_ext_batch[0] = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-            
-            self.solver.solver.set_external_wrench_batch(self.solver.f_ext_batch)
-                
-        return best_idx
+        self.stats_save_timer = self.create_timer(STATS_SAVE_INTERVAL, self.save_stats)
+        self.last_save_time = time.time()
         
     def joint_callback(self, msg):
+        x_curr = np.hstack([np.array(msg.position), np.array(msg.velocity)])
+        ee_pos = np.array(msg.effort[0:3])
+        
         if self.x_last is None:
-            self.x_last = np.hstack([np.array(msg.position), np.array(msg.velocity)])
-            self.u_last = np.zeros(6)
+            self.x_last, self.u_last = x_curr, np.zeros(6)
         
         # timing
         elapsed_time = time.time() - self.last_time
-        self.delta_ts.append(elapsed_time)
+        self.dts.append(elapsed_time)
         self.last_time = time.time()
+        
+        # ----- update batched inputs -----
         self.ref_traj_offset += elapsed_time/self.dt
         offset = int(self.ref_traj_offset)
-        
-        # update batched states
-        xs = np.hstack([np.array(msg.position), np.array(msg.velocity)])
-        self.xs_batch[:,:] = xs
-        
-        # update batched ee pos goal
         self.ee_pos_traj_batch[:,:] = self.ref_traj[6*offset:6*(offset+self.N)]
+        self.xs_batch[:,:] = x_curr
+        self.XU_batch[:, 0:self.nx] = x_curr
         
-        # update batched XU
-        self.XU_batch[:,:] = self.XU_best
-        self.XU_batch[:, 0:self.nx] = xs
-        print(f"xs: {xs}")
-        
-        # solve
-        start_time = time.time()
+        # ----- solve -----
         self.solver.reset()
         #self.solver.solver.resetRho()
         #self.solver.solver.resetLambda()
-        XU_batch_new = self.solver.solve(self.xs_batch, self.ee_pos_traj_batch, self.XU_batch)
-        end_time = time.time()
-        #print(f"batch sqp time: {1000 * (end_time - start_time)} ms")
-        #print(f"step sizes: {step_sizes}")
-        
-        best_idx = self.find_best_trajectory_id(self.x_last, self.u_last, xs, elapsed_time)
+        XU_batch_new, solve_time = self.solver.solve(self.xs_batch, self.ee_pos_traj_batch, self.XU_batch)
+        best_idx = self.solver.find_best_idx(self.x_last, self.u_last, x_curr, elapsed_time)
+        self.solver.resample_f_ext_batch(best_idx)
         self.XU_best = XU_batch_new[best_idx]
-        u = self.XU_best[self.nx:(self.nx+self.nu)]
-        print(f"u: {u}")
-        print(f"best_idx: {best_idx}")
+        u_curr = self.XU_best[self.nx:(self.nx+self.nu)]
         
+        # ----- publish control -----
         self.ctrl_msg.header.stamp = self.get_clock().now().to_msg()
-        self.ctrl_msg.position = [0.0] * 6 
-        self.ctrl_msg.velocity = [0.0] * 6
-        self.ctrl_msg.effort = [float(e) for e in u]
+        self.ctrl_msg.effort = [float(e) for e in u_curr]
         self.publisher.publish(self.ctrl_msg)
         
+        # ----- log stats and update -----
+        tracking_error = np.linalg.norm(ee_pos - self.ee_pos_traj_batch[0, :3])
+        self.tracking_errors.append(tracking_error)
+        self.ee_positions.append(ee_pos)
+        self.ee_ref_positions.append(self.ee_pos_traj_batch[0, :3])
+        self.joint_positions.append(x_curr[:6])
+        self.solve_times.append(solve_time)
         
-        # self.fig8_offset += 6
-        # self.fig8_offset %= len(self.fig8)
-        # copy1_len = min(self.fig8_offset + 6*self.N, len(self.fig8)) - self.fig8_offset
-        # copy2_len = 6*self.N - copy1_len
-        # self.eepos_g_batch[:,:copy1_len] = self.fig8[self.fig8_offset:self.fig8_offset+copy1_len]
-        # if copy2_len > 0:
-        #     self.eepos_g_batch[:,-copy2_len:] = self.fig8[:copy2_len]
-            
-        self.x_last = xs
-        self.u_last = u
+        self.XU_batch[:,:] = self.XU_best
+        self.x_last, self.u_last = x_curr, u_curr
         
-        print("\n--------------------------------\n\n")
+        print(f"best idx: {best_idx}  ----- f_ext: {[f'{x:.4f}' for x in self.solver.f_ext_batch[best_idx][:3]]}")
+        print(f"q: {[f'{x:.4f}' for x in x_curr[:6]]}")
+        print(f"u: {[f'{x:.4f}' for x in u_curr]}")
+        print(f"time elapsed: {elapsed_time:.4f}s")
+        print("\n--------------------------------\n")
         
-        
-    def send_external_force(self):
-        force = self.f_ext[0:3]
-        torque = self.f_ext[3:6]
-        
+    def send_external_force(self, wrench):
+        self.f_ext_actual = wrench
         wrench_msg = Wrench()
-        wrench_msg.force.x = float(force[0])
-        wrench_msg.force.y = float(force[1])
-        wrench_msg.force.z = float(force[2])
-        wrench_msg.torque.x = float(torque[0])
-        wrench_msg.torque.y = float(torque[1])
-        wrench_msg.torque.z = float(torque[2])
-        
+        wrench_msg.force.x = float(wrench[0])
+        wrench_msg.force.y = float(wrench[1])
+        wrench_msg.force.z = float(wrench[2])
+        wrench_msg.torque.x = float(wrench[3])
+        wrench_msg.torque.y = float(wrench[4])
+        wrench_msg.torque.z = float(wrench[5])
         self.external_force_publisher.publish(wrench_msg)
-        self.get_logger().info(f'Sent external force: {force}, torque: {torque}')
+        self.get_logger().info(f'Sent external wrench: {wrench}')
+
+    def save_stats(self):
+        current_time = time.time()
+        elapsed_since_last_save = current_time - self.last_save_time
+        
+        if elapsed_since_last_save >= STATS_SAVE_INTERVAL:
+            self.last_save_time = current_time
+
+            timestamp = datetime.now().strftime("%H%M%S")
+            base_filename = os.path.join("stats/", f"{timestamp}")
+            
+            # Convert lists to numpy arrays
+            dts = np.array(self.dts)
+            tracking_errors = np.array(self.tracking_errors)
+            ee_positions = np.array(self.ee_positions)
+            ee_ref_positions = np.array(self.ee_ref_positions)
+            joint_positions = np.array(self.joint_positions)
+            solve_times = np.array(self.solve_times)
+            
+            # Save each array to a separate .npy file
+            np.save(f"{base_filename}_dts.npy", dts)
+            np.save(f"{base_filename}_tracking_errors.npy", tracking_errors)
+            np.save(f"{base_filename}_ee_positions.npy", ee_positions)
+            np.save(f"{base_filename}_ee_ref_positions.npy", ee_ref_positions)
+            np.save(f"{base_filename}_joint_positions.npy", joint_positions)
+            np.save(f"{base_filename}_solve_times.npy", solve_times)
+            
+            self.get_logger().info(f'Statistics saved to {base_filename}_*.npy files')
+            
         
     def check_timeout(self):
         current_time = time.time()
@@ -278,16 +278,33 @@ class GATO_Controller(Node):
             rclpy.shutdown()
             sys.exit(1)
             
+
 def main(args=None):
     try:
         rclpy.init(args=args)
+
+        dt = 0.03
+        N = 32
+        f_ext_actual = [10.0, 20.0, -30.0, 0.0, 0.0, 0.0]
+        ref_traj = figure8(A_x=0.4, A_z=0.4, 
+                           offset=[0.0, 0.5, 0.6], 
+                           period=10, 
+                           dt=dt, 
+                           cycles=10)
         
-        ref_traj = figure8(A_x=0.4, A_z=0.4, offset=[0.0, 0.50, 0.7], period=5, dt=0.01, cycles=5)
+        # ----- 32 BATCH SAMPLE CONFIG -----
+
+        gato_controller = GATO_Controller(ref_traj=ref_traj, batch_size=64, N=N, dt=dt,
+                                          f_ext_std=2.0, f_ext_resample_std=0.5, 
+                                          f_ext_actual=f_ext_actual)
         
-        f_ext_actual = [5.0, 5.0, 0.0, 0.0, 0.0, 0.0]
+        # ----- 64 BATCH SAMPLE CONFIG -----
+
+        # gato_controller = GATO_Controller(ref_traj=ref_traj, batch_size=64, N=N, dt=dt,
+        #                                   f_ext_std=5.0, f_ext_resample_std=1.0, 
+        #                                   f_ext_actual=f_ext_actual)
         
-        gato_controller = GATO_Controller(ref_traj=ref_traj, batch_size=32, N=32, dt=0.01,
-                                          f_ext_std=0.0, f_ext_actual=f_ext_actual)
+ 
         rclpy.spin(gato_controller)
     except KeyboardInterrupt:
         print("\nShutting down gracefully...")
